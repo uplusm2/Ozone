0276bfd0da7b1ba7dd1b216099f8a56ef272c73a
Hwang-97 <qqq4472@gmail.com>
2022-02-22 14:28:45 +0900

    feat: Create Spring Boot project files
    Create a project file so that you can also code in Spring Boot

---
+++ OzoneSpringBoot/OzoneSpringBoot/src/main/java/com/test/OzoneSpringBoot/OzoneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.test.OzoneSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzoneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzoneSpringBootApplication.class, args);
+	}
+
+}
---
+++ OzoneSpringBoot/OzoneSpringBoot/src/test/java/com/test/OzoneSpringBoot/OzoneSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.OzoneSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzoneSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
